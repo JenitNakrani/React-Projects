1c847c802a5cadd625089704647a2e7e00024d6d
AzureAD\JenitNakrani <jenit.nakrani@thecommerceteam.com>
2023-08-10 17:52:05 +0530

    add  files

---
+++ Key-Concept-Page/src/App.js
@@ -0,0 +1,17 @@
+import keyConceptsImage from './assets/images/key-concepts.png';
+import Concepts from './concepts/Concepts';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
+      </header>
+      <Concepts />
+    </div>
+  );
+}
+
+export default App;
